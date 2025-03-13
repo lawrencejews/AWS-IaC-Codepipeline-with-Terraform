671c34da37c09b0c7df723adf955e611b847c533
lawrencejews <lawrencejews1@gmail.com>
2025-03-13 21:38:11 +0900

    updated the autoscaling groups

--- terraform-manifests/c13-06-autoscaling-ttsp.tf
+++ terraform-manifests/c13-06-autoscaling-ttsp.tf
@@ -26,14 +26,8 @@ resource "aws_autoscaling_policy" "alb_target_requests_greater_than_yy" {
   target_tracking_configuration {
     predefined_metric_specification {
       predefined_metric_type = "ALBRequestCountPerTarget"
-      #resource_label =  "${module.alb.lb_arn_suffix}/${module.alb.target_group_arn_suffixes[0]}"    
-      resource_label =  "${module.alb.arn_suffix}/${module.alb.target_groups["mytg1"].arn_suffix}"  # UPDATED NOV2023      
+      resource_label =  "${module.alb.lb_arn_suffix}/${module.alb.target_group_arn_suffixes[0]}"    
     }  
     target_value = 10.0
   }    
-}
-
-# Updated Nov2023
-output "asg_build_resource_label" {
-  value =  "${module.alb.arn_suffix}/${module.alb.target_groups["mytg1"].arn_suffix}"  
 }
