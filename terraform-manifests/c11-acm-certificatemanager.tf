ad9cf15e90692c5581cea1d258c60fb735e201cf
lawrencejews <lawrencejews1@gmail.com>
2025-03-14 19:21:13 +0900

    dns version error testing

--- terraform-manifests/c11-acm-certificatemanager.tf
+++ terraform-manifests/c11-acm-certificatemanager.tf
@@ -1,14 +1,14 @@
 # ACM Module - To create and Verify SSL Certificates
 module "acm" {
   source  = "terraform-aws-modules/acm/aws"
-  version = "5.1.1"
+  version = "3.0.0"
 
-  domain_name  = trimsuffix(data.aws_route53_zone.mydomain.name, ".")
-  zone_id      = data.aws_route53_zone.mydomain.zone_id 
+  domain_name = trimsuffix(data.aws_route53_zone.mydomain.name, ".")
+  zone_id     = data.aws_route53_zone.mydomain.zone_id
 
   subject_alternative_names = [
     #"*.lubdevops.pro"    -> Referencing one DNS NAME
-    var.dns_name     # Referencing Multiple DNS NAME
+    var.dns_name
   ]
   tags = local.common_tags
 }
